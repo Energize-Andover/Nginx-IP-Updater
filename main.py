29176e3599fd0146adc250e50f5b6177979e4e8c
Daniel Ivanovich <dan@ivanovi.ch>
2019-07-28 18:07:30 -0400

    Finish main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import sys
 from helpers import *
 from secrets import *
 
+CONFIG_FILE_FOLDER = path_join(os.sep, 'etc', 'nginx', 'conf.d')
+
 
 def get_inet_ip():
     net_config = run_command('ifconfig', '-a')[0]
@@ -20,6 +22,11 @@ def get_inet_ip():
 
 
 if __name__ == "__main__":
+
+    if os.geteuid() != 0:
+        print("Please run this file as sudo!")
+        sys.exit(-1)
+
     last_ip = get_inet_ip()
 
     try:
@@ -27,9 +34,51 @@ if __name__ == "__main__":
             current_ip = get_inet_ip()
 
             if current_ip != last_ip:
-                print('changed')
+                for file in CONF_FILE_NAMES:
+                    file_path = os.path.join(CONFIG_FILE_FOLDER, file)
+
+                    file_data = None
+
+                    with open(file_path, 'r') as config_file:
+                        file_data = config_file.read()
+
+                    original_file_data = file_data
+
+                    # Replace all instances of the old ip with the new ips
+                    file_data = file_data.replace(last_ip, current_ip)
+
+                    # Delete old config file
+                    os.remove(file_path)
+
+                    # Replace with new config file
+                    with open(file_path, 'w+') as config_file:
+                        config_file.write(file_data)
+
+                    # Test config
+                    test_output = run_command('sudo', 'nginx', '-t')[0].decode('UTF-8')
+
+                    passed_test = True
+
+                    for output_line in test_output.split('\n'):
+                        if len(output_line) > 0:
+                            if not output_line.endswith('is ok') and not output_line.endswith('is successful'):
+                                passed_test = False
+                                break
+
+                    if not passed_test:
+                        os.remove(file_path)  # Remove modified config file
+
+                        # Restore to original state
+                        with open(file_path, 'w+') as config_file:
+                            config_file.write(original_file_data)
+
+                        print("There are problems with your configuration syntax in the file " + file_path)
+                        sys.exit(-1)
+
+                    # Passed the test, restart
+                    run_command('sudo', 'systemctl', 'restart', 'nginx')
 
             last_ip = current_ip
             time.sleep(60)  # Run every minute
     except KeyboardInterrupt:
-        sys.exit(1)
+        sys.exit(-1)
